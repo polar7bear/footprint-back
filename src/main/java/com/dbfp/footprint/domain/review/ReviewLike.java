e7f3c9921bd087f5f56a5b4235f5c5cedbc1abdc
polarbear <sons1998@naver.com>
2024-03-13 11:35:53 +0900

    [FP-37] 리뷰 도메인 엔티티 설계

---
+++ src/main/java/com/dbfp/footprint/domain/review/ReviewLike.java
@@ -0,0 +1,26 @@
+package com.dbfp.footprint.domain.review;
+
+import com.dbfp.footprint.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "review_like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "review_id")
+    private Review review;
+}
