8f5de30c2fb4a02786dde9cb71ab3f8be379f72b
ruth1442 <ruth1442@naver.com>
2024-03-19 16:48:21 +0900

    [FP-61] s3 의존성 merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 
 
+//    implementation("org.springframework.boot:spring-boot-starter-validation")
+//    implementation("org.hibernate.hibernate-validator:4.3.0.Final")
 //    testImplementation("org.springframework.boot:spring-boot-starter-test")
 //    testImplementation("org.springframework.security:spring-security-test")
 }
