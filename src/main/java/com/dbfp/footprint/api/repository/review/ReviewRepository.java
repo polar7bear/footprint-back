5008d3d3b990532a2dc9e6680ee7eb2e81dee128
ruth1442 <ruth1442@naver.com>
2024-04-02 16:17:22 +0900

    [FP-51] 리뷰 검색기능 추가, 테스트중

--- src/main/java/com/dbfp/footprint/api/repository/review/ReviewRepository.java
+++ src/main/java/com/dbfp/footprint/api/repository/review/ReviewRepository.java
@@ -12,4 +12,7 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllByMember(Member member, PageRequest pageRequest);
+
+    // 리뷰 검색기능 (findBy(컬럼이름)Containing -> 컬럼에서 키워드가 포함된 것을 찾겠다. 글자 한 글자만 검색해도 그 글자가 포함된 것은 다 나옴)
+    Page<Review> findByTitleContainingOrContentContaining(String searchKeyword, PageRequest reviewId);
 }
