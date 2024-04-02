c069f74e7ed1459fd28aa254ecccbe4091f65caa
s-kang <seok550264@gmail.com>
2024-04-02 18:22:26 +0900

    [FP-78] 코드 정리, Plan응답 수정

--- src/main/java/com/dbfp/footprint/api/controller/plan/PlanBookmarkController.java
+++ src/main/java/com/dbfp/footprint/api/controller/plan/PlanBookmarkController.java
@@ -1,17 +1,9 @@
 package com.dbfp.footprint.api.controller.plan;
 
-import com.dbfp.footprint.api.response.ApiError;
 import com.dbfp.footprint.api.response.ApiResult;
 import com.dbfp.footprint.api.service.plan.PlanBookmarkService;
 import com.dbfp.footprint.dto.PlanBookmarkDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
