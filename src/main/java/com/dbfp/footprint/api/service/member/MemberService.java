5b1718c97ab25badfaef005ffe4229611b39fc00
ruth1442 <ruth1442@naver.com>
2024-04-09 13:17:04 +0900

    [FP-85] 임시 멤버 생성 api 추가

--- src/main/java/com/dbfp/footprint/api/service/member/MemberService.java
+++ src/main/java/com/dbfp/footprint/api/service/member/MemberService.java
@@ -1,6 +1,8 @@
 package com.dbfp.footprint.api.service.member;
 
 import com.dbfp.footprint.api.repository.member.MemberRepository;
+import com.dbfp.footprint.api.request.member.CreateMemberRequest;
+import com.dbfp.footprint.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,11 @@ public class MemberService {
 
     public Long signUp() {
         return null;
+    }
+
+    public Long createMember(CreateMemberRequest createMemberRequest) {
+        Member member = Member.of(createMemberRequest);
+        memberRepository.save(member);
+        return member.getId();
     }
 }
