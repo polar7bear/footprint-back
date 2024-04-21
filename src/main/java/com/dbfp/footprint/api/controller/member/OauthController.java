2d760331ceb95c548a1971b6ebeffa483be5376d
polarbear <sons1998@naver.com>
2024-04-22 00:08:47 +0900

    [FP-90] 카카오 로그인 구현

---
+++ src/main/java/com/dbfp/footprint/api/controller/member/OauthController.java
@@ -0,0 +1,34 @@
+package com.dbfp.footprint.api.controller.member;
+
+import com.dbfp.footprint.api.response.LoginMemberResponse;
+import com.dbfp.footprint.api.service.member.OauthService;
+import com.dbfp.footprint.config.jwt.JwtFilter;
+import com.dbfp.footprint.domain.Member;
+import com.dbfp.footprint.dto.oauth.KakaoProfile;
+import com.dbfp.footprint.dto.oauth.OAuthToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/kakao")
+public class OauthController {
+
+    private final OauthService oauthService;
+
+    @GetMapping("/callback")
+    public ResponseEntity<LoginMemberResponse> callback(@RequestParam String code) throws JsonProcessingException {
+        OAuthToken token = oauthService.getKakaoOAuthToken(code);
+        KakaoProfile profile = oauthService.getKakaoProfile(token.getAccess_token());
+        LoginMemberResponse response = oauthService.registerOrLoginUser(profile);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + response.getAccessToken());
+
+        return new ResponseEntity<>(response, httpHeaders, HttpStatus.OK);
+    }
+}
