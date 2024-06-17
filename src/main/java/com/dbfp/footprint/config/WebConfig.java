2c4dba48c2de2c450e01025d14ef75f8fde13f1b
polarbear <sons1998@naver.com>
2024-06-17 16:14:43 +0900

    Fix: URL 수정

--- src/main/java/com/dbfp/footprint/config/WebConfig.java
+++ src/main/java/com/dbfp/footprint/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("dlb9mfb1kq1mn.cloudfront.net")
+                .allowedOrigins("http://dlb9mfb1kq1mn.cloudfront.net")
                 .allowedMethods("*")
                 .allowedHeaders("*");
 
