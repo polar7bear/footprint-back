3d1fa882e22a6a771392f82ebb9cd1458fa01a26
polarbear <sons1998@naver.com>
2024-06-18 14:05:09 +0900

    Fix: 도메인명 수정

--- src/main/java/com/dbfp/footprint/config/WebConfig.java
+++ src/main/java/com/dbfp/footprint/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://dlb9mfb1kq1mn.cloudfront.net")
+                .allowedOrigins("https://dfbfootprint.site")
                 .allowedMethods("*")
                 .allowedHeaders("*");
 
